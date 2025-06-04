3ea71c6127b362802e505f2079a9b48029e832ac
sokashop <65soka@gmail.com>
2025-06-04 22:59:42 +0100

    Add files via upload

---
+++ assets/js/basket.js
@@ -0,0 +1,63 @@
+if(localStorage.getItem("basket") !== null){
+    let basket= JSON.parse(localStorage.getItem("basket"));
+    document.querySelector("main>div:first-child").innerHTML+=` (${basket.length})`;
+    basket.forEach((e,i) => {
+        let div0=document.createElement("div");
+        let img=document.createElement("img");
+        img.src=`/assets/images/${e.path}`;
+        let span=document.createElement("span");
+        span.classList.add("close");
+        span.onclick=(sp)=>{
+            sp.target.parentNode.style.display="none";
+            basket.splice(i,1);
+            document.querySelector("main>div:first-child").innerHTML=`سلتي (${basket.length})`;
+            localStorage.setItem("basket",JSON.stringify(basket));
+        };
+        let title=document.createElement("div");
+        title.classList.add("title");
+        title.appendChild(document.createTextNode(e.name));
+        let price=document.createElement("div");
+        price.classList.add("price");
+        price.appendChild(document.createTextNode(`${e.price} DH`));
+        let adiv=document.createElement("div");
+        let cs=document.createElement("div");
+        let color=document.createElement("div");
+        color.classList.add("color");
+        color.style.backgroundColor=e.color;
+        let size=document.createElement("div");
+        size.classList.add("size");
+        size.appendChild(document.createTextNode(e.size));
+        cs.appendChild(color);
+        cs.appendChild(size);
+        div0.appendChild(span);
+        div0.appendChild(img);
+        adiv.appendChild(title);
+        adiv.appendChild(price);
+        adiv.appendChild(cs);
+        div0.appendChild(adiv);
+        document.querySelector("div.myp").appendChild(div0);
+    });
+}
+
+document.querySelector("button.order").onclick=()=>{
+    // alert("A");
+    if(localStorage.getItem("basket") !== null){
+        let basket= JSON.parse(localStorage.getItem("basket"));
+        let st="";
+        let pth="https://sokashop.github.io/assets/images/";
+        basket.forEach((v,i)=>{
+            if(i > 0)st+=`%0A_________________%0A`;
+            st+=`
+Name  : ${v.name}%0A
+Price : ${v.price} DH%0A
+Image : ${pth}${v.path}%0A
+Color : ${v.color}%0A
+Size  : ${v.size}
+            `;
+        });
+        let link=`https://wa.me/212698203145?text=${st}`;
+        open(link);
+    }
+}
+
+document.querySelector("footer div.copy").innerHTML=`&copy ${new Date().getFullYear()}`;
---
+++ assets/js/master.js
@@ -0,0 +1,106 @@
+if(localStorage.getItem("basket") === null || JSON.parse(localStorage.getItem("basket")).length === 0){
+    document.querySelector("div.basket div.bnumber").style.opacity="0";
+}
+else{
+    document.querySelector("div.basket div.bnumber").innerHTML=JSON.parse(localStorage.getItem("basket")).length;
+    document.querySelector("div.basket div.bnumber").style.opacity="1";
+}
+document.querySelector("div.viewP div.close").onclick=(el)=>{
+    el.target.parentNode.parentNode.style.display="none";
+        document.body.style.overflowY="scroll";
+};
+
+let pAr=[];
+fetch("/assets/p.json")
+    .then((res)=>res.json())
+    .then((data)=>{
+        pAr=data;
+        pAr.forEach((p,i)=>{
+            let div0=document.createElement("div");
+            div0.classList.add("prod");
+            let img=document.createElement("img");
+            img.src=`/assets/images/${p.path}`;
+            let div1=document.createElement("div");
+            div1.classList.add("title");
+            div1.appendChild(document.createTextNode(p.name));
+            let div2=document.createElement("div");
+            div2.classList.add("price");
+            div2.appendChild(document.createTextNode(`${p.price} DH`));
+            div0.appendChild(img);
+            div0.appendChild(div1);
+            div0.appendChild(div2);
+            div0.onclick=function(){
+                let btAr={};
+                document.querySelector("div.viewP div.addbtn").style.opacity=".5";        
+                document.querySelector("div.viewP div.addbtn").style.cursor="not-allowed";                
+                document.querySelector("div.viewP").style.display="flex";
+                document.querySelector("div.viewP").setAttribute("imgI",i);
+                document.querySelector("div.viewP img").src=`/assets/images/${p.path}`;
+                document.querySelector("div.viewP div.t").innerHTML=p.name;
+                document.querySelector("div.viewP div.p").innerHTML=`${p.price} DH`;
+                document.querySelector("div.viewP div.colors").innerHTML="";
+                p.colors.forEach((c)=>{
+                    let spanc=document.createElement("span");
+                    spanc.style.backgroundColor=c;
+                    spanc.onclick=()=>{
+                        btAr.color=c;
+                        document.querySelectorAll("div.viewP div.colors span").forEach((el)=>{
+                            el.style.opacity=".5";
+                        });
+                        spanc.style.opacity="1";
+                        if (Object.keys(btAr).length >= 2){
+                            document.querySelector("div.viewP div.addbtn").style.opacity="1";
+                            document.querySelector("div.viewP div.addbtn").style.cursor="pointer";  
+                        }
+                    };
+                    document.querySelector("div.viewP div.colors").appendChild(spanc);
+                });
+                document.querySelector("div.viewP div.sizes").innerHTML="";
+                p.sizes.forEach((s)=>{
+                    let spans=document.createElement("span");
+                    spans.appendChild(document.createTextNode(s));
+                    spans.onclick=()=>{
+                        btAr.size=s;
+                        document.querySelectorAll("div.viewP div.sizes span").forEach((el)=>{
+                            el.style.textDecoration="none";
+                        });
+                        spans.style.textDecoration="line-through";
+                        if (Object.keys(btAr).length >= 2){
+                            document.querySelector("div.viewP div.addbtn").style.opacity="1";
+                            document.querySelector("div.viewP div.addbtn").style.cursor="pointer";
+                        }
+                    };
+                    document.querySelector("div.viewP div.sizes").appendChild(spans);
+                });
+                
+                document.querySelector("div.viewP div.addbtn").onclick=(btn)=>{
+                    if(btn.target.style.opacity === "1"){
+                        if(localStorage.getItem("basket") === null){
+                            localStorage.setItem("basket",JSON.stringify([{name:p.name,price:p.price,path:p.path,color:btAr.color,size:btAr.size}]));
+                            document.querySelector("div.basket div.bnumber").innerHTML= "1";
+                        }
+                        else{
+                            let baskett=JSON.parse(localStorage.getItem("basket"));
+                            baskett.push({name:p.name,price:p.price,path:p.path,color:btAr.color,size:btAr.size});
+                            localStorage.setItem("basket",JSON.stringify(baskett));
+                            document.querySelector("div.basket div.bnumber").innerHTML=baskett.length;
+                        }
+                        document.querySelector("div.basket div.bnumber").style.opacity="1";
+                        document.querySelectorAll("div.viewP div.sizes span").forEach((el)=>{
+                                el.style.textDecoration="none";
+                            });
+                        document.querySelectorAll("div.viewP div.colors span").forEach((el)=>{
+                                el.style.opacity=".5";
+                            });
+                        btn.target.style.opacity=".5";
+                        btn.target.style.cursor="not-allowed";
+                        btAr={};
+                    }
+                };
+                document.body.style.overflowY="hidden";
+            };
+            document.querySelector("div.product").appendChild(div0)
+        })
+    });
+
+document.querySelector("footer div.copy").innerHTML=`&copy ${new Date().getFullYear()}`;
